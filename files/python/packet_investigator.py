0227d69bf905b1f9d4a6b3bc03b2220d2930dc70
Veronica Zvereva <veronicazvereva@Veronicas-MacBook-Pro.local>
2020-09-29 15:02:59 +0300

    rebuilding site Tue Sep 29 15:02:59 MSK 2020

--- files/python/packet_investigator.py
+++ files/python/packet_investigator.py
@@ -1,15 +1,28 @@
 import sys
 from scapy.all import *
+from pcapy import open_offline, open_live
+from impacket.ImpactDecoder import EthDecoder
 
 
-class scapy_demo(object):
+class packet_investigator(object):
     def __init__(self):
-        super(scapy_demo, self).__init__()
+        super(packet_investigator, self).__init__()
         pass
 
     def main(self, pcap_file):
         pcap = rdpcap(pcap_file)
         print(pcap.sessions())
+
+    def read_packet(self, hdr, data):
+        decoder = EthDecoder()
+        ether =  decoder.decode(data)
+        ip = ether.child()
+        transport = ip.child()
+        print(transport)
+
+    def impact_demo(self, pcap_file):
+        pcap = open_offline(pcap_file)
+        pcap.loop(0, self.read_packet)
 
     def find_unique_ips(self, pcap_file):
         pcap = rdpcap(pcap_file)
@@ -33,13 +46,15 @@ class scapy_demo(object):
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
-        sys.exit("Usage: python scapy file.pcap [unique_ips]")
-    scapy_parser = scapy_demo()
+        sys.exit("Usage:  %s [file].pcap [-unique_ips] [-susp_ip]" % sys.argv[0])
+    pckt_investigator = packet_investigator()
     if sys.argv.__contains__("-unique_ips"):        
-        scapy_parser.find_unique_ips(sys.argv[1])
+        pckt_investigator.find_unique_ips(sys.argv[1])
     if sys.argv.__contains__("-susp_ip"):
-        scapy_parser.find_packets_with_ip(sys.argv[1], sys.argv[3])
+        pckt_investigator.find_packets_with_ip(sys.argv[1], sys.argv[3])
+    if sys.argv.__contains__("impact_demo"):
+        pckt_investigator.impact_demo(sys.argv[1])
     else:
-        scapy_parser.main(sys.argv[1])
+        pckt_investigator.main(sys.argv[1])
 
     
